9c70f64115a9f8f00e45b0d065bcce262a53a0a2
ringo156 <blackpple262@gmail.com>
2016-12-31 01:37:40 +0900

    とりあえず動いた

---
+++ PictCorect/correct.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import get_image
+
+classgetimage = get_image.getImage()
+extensions = ".jpg,.jpeg,.png"
+url = ''
+f = open('source.txt', 'rt')
+texts = f.readlines() # 1行毎にファイル終端まで全て読む(改行文字も含まれる)
+f.close()
+
+for text in texts:
+	# クロール開始
+    url = text.replace('\n', '')
+    classgetimage.crawring(url, extensions)
